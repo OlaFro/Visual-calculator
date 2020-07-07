d171dd8d4bc57df457c0f7c62d5821bd021713e2
olafro <olka.be@gmail.com>
2020-07-07 16:29:12 +0200

    second

---
+++ script.js
@@ -0,0 +1,45 @@
+display(){
+    for (i=1; i<=9; i++){
+        switch(i) {
+            case 1 (
+                document.getElementById("display").innerHTML = "1";
+                break;
+            )
+            case 2 (
+                document.getElementById("display").innerHTML = "2";
+                break;
+            )
+            case 3 (
+                document.getElementById("display").innerHTML = "3";
+                break;
+            )
+            case 4 (
+                document.getElementById("display").innerHTML = "4";
+                break;
+            )
+            case 5 (
+                document.getElementById("display").innerHTML = "5";
+                break;
+            )
+            case 6 (
+                document.getElementById("display").innerHTML = "6";
+                break;
+            )
+            case 7 (
+                document.getElementById("display").innerHTML = "7";
+                break;
+            )
+            case 8 (
+                document.getElementById("display").innerHTML = "8";
+                break;
+            )
+            case 9 (
+                document.getElementById("display").innerHTML = "9";
+                break;
+            )
+            
+        }
+
+    }
+    
+}
